1f4863c17f61a45aad15e4fa4d70e835b7094da0
Jocker00 <grove1411@gmail.com>
2022-03-18 18:32:42 -0300

    First deploy

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      message: ""
+    }
+  }
+  
+
+  componentDidMount = () => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => this.setState(data))
+  }
+
+  render() {
+    return (
+      <div>{this.state.message}</div>
+    )
+  }
+}
+
+export default App
